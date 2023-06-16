fe1a1a5a2507900a2a4fdc16e5d9445f9ba235c2
anwar <a.ammarr17@gmail.com>
2023-06-16 07:57:34 -0700

    push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,8 @@
 /**
  * main - assigns a random number to int n everytime
  * it executes, and prints it
- * Return : return 0 (success) 
- **/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
