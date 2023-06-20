72ea5dc099f6749701958b73438e8bdd4a1586da
anwar <a.ammarr17@gmail.com>
2023-06-20 04:40:10 -0700

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,12 @@
  */
 void print_alphabet(void)
 {
-	
-        char letter;
+	char letter = 'a';
 
-	for(letter = 'a';letter <='z';letter++)
+	while (letter <= 'z')
 	{
 		_putchar(letter);
-		
+		letter++;
 	}
 	_putchar('\n');
 }
