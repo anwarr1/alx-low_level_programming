e9965a44ba1e217a5a769c13a32dfedc837d3f06
anwar <a.ammarr17@gmail.com>
2023-08-31 06:41:41 -0700

    done

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+why those are defined in a separate file main.h :  #ifndef _MAIN_H_
+#define _MAIN_H_
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
+
+
+#endif
